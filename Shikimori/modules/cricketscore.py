6828c2efa7543b421255d41ae365006e985d97cf
Chickhunter <108670549+Chickhunter@users.noreply.github.com>
2022-07-14 19:02:50 +0530

    Update cricketscore.py

--- Shikimori/modules/cricketscore.py
+++ Shikimori/modules/cricketscore.py
@@ -40,7 +40,7 @@ async def _(event):
         parse_mode="HTML",
     )
 
-__mod_name__ = "Cricket Score 🏏"
+__mod_name__ = "《Cricket Score》"
 __help__ = """
 *Cricket Score*
  ❍ `/c` : To check live cricket score
