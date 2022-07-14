7ebb035c984cc1f41cebc793422239b67db94fd5
Chickhunter <108670549+Chickhunter@users.noreply.github.com>
2022-07-14 19:04:43 +0530

    Update cust_filters.py

--- Shikimori/modules/cust_filters.py
+++ Shikimori/modules/cust_filters.py
@@ -630,7 +630,7 @@ doin?
 Check /markdownhelp to know more!
 """
 
-__mod_name__ = "Filters"
+__mod_name__ = "《Filters》"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
